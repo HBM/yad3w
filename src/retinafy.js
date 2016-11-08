adfe23b649678e1b7cbd08ce04b6cf984ab7e002
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-11-08 09:21:03 +0100

    fix standard

--- src/retinafy.js
+++ src/retinafy.js
@@ -1,30 +1,29 @@
 
 // https://www.html5rocks.com/en/tutorials/canvas/hidpi/
 const retinafy = (canvas) => {
+  let context = canvas.getContext('2d')
 
-    let context = canvas.getContext('2d')
+  const devicePixelRatio = window.devicePixelRatio || 1
+  const backingStoreRatio =
+    context.webkitBackingStorePixelRatio ||
+    context.mozBackingStorePixelRatio ||
+    context.msBackingStorePixelRatio ||
+    context.oBackingStorePixelRatio ||
+    context.backingStorePixelRatio || 1
 
-    const devicePixelRatio = window.devicePixelRatio || 1
-    const backingStoreRatio =
-      context.webkitBackingStorePixelRatio ||
-      context.mozBackingStorePixelRatio ||
-      context.msBackingStorePixelRatio ||
-      context.oBackingStorePixelRatio ||
-      context.backingStorePixelRatio || 1
+  const ratio = devicePixelRatio / backingStoreRatio
 
-    const ratio = devicePixelRatio / backingStoreRatio
+  if (devicePixelRatio !== backingStoreRatio) {
+    canvas.style.width = `${canvas.width}px`
+    canvas.style.height = `${canvas.height}px`
 
-    if (devicePixelRatio !== backingStoreRatio) {
-      canvas.style.width = `${canvas.width}px`
-      canvas.style.height = `${canvas.height}px`
+    canvas.width *= ratio
+    canvas.height *= ratio
 
-      canvas.width *= ratio
-      canvas.height *= ratio
+    context.scale(ratio, ratio)
+  }
 
-      context.scale(ratio, ratio)
-    }
-
-    return canvas
+  return canvas
 }
 
 export default retinafy

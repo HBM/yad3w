d9dc54c58851bb6fce64049dfe5749e8c747d686
Mirco Zeiss <mirco.zeiss@gmail.com>
2017-04-25 16:24:08 +0200

    add special cases for all pos, all neg and in between

--- src/Barchart.js
+++ src/Barchart.js
@@ -12,7 +12,7 @@ const defaults = {
     top: 20,
     right: 20,
     bottom: 20,
-    left: 20
+    left: 30
   },
   yTicks: 5,
   tickFormat: d => '-' + ((Date.now() - d) / 1000).toFixed(1) + ' s'
@@ -24,7 +24,7 @@ export default class Barchart {
 
     const {target, width, height, margin} = this
     const w = width - margin.left - margin.right
-    const h = height - margin.top - margin.bottom
+    this.h = height - margin.top - margin.bottom
 
     const {yTicks} = this
 
@@ -39,7 +39,7 @@ export default class Barchart {
       .padding(0.1)
 
     this.y = scaleLinear()
-      .rangeRound([h, 0])
+      .rangeRound([this.h, 0])
 
     this.xAxis = axisBottom(this.x)
 
@@ -56,7 +56,10 @@ export default class Barchart {
   render (data) {
     const {x, y, xAxis, yAxis, chart, tickFormat} = this
 
-    y.domain(extent(data, v => v.value))
+    const extented = extent(data, v => v.value)
+    const [min, max] = extented
+
+    y.domain(extented)
 
     const domain = data.map(d => d.timestamp)
     x.domain(domain)
@@ -80,15 +83,42 @@ export default class Barchart {
     // enter selection
     // after merging entered elements with the update selection,
     // apply operations to both
+
+    // all positive
+    // .attr('y', d => y(d.value))
+    // .attr('height', d => this.h - y(d.value))
+    // pos and neg
+    // .attr('y', d => y(Math.max(0, d.value)))
+    // .attr('height', d => Math.abs(y(d.value) - y(0)))
+    // all neg
+    // .attr('y', d => y(ma))
+    // .attr('height', d => y(d.value))
+
+    let yFn
+    let heightFn
+    if (min >= 0) {
+      // all positive
+      yFn = d => y(d.value)
+      heightFn = d => this.h - y(d.value)
+    } else if (max <= 0) {
+      // all negative
+      yFn = d => y(max)
+      heightFn = d => y(d.value)
+    } else {
+      // some positive and some negative
+      yFn = d => y(Math.max(0, d.value))
+      heightFn = d => Math.abs(y(d.value) - y(0))
+    }
+
     bars
       .enter()
       .append('rect')
       .attr('class', 'bar')
       .merge(bars)
       .attr('x', d => x(d.timestamp))
-      .attr('y', d => y(Math.max(0, d.value)))
+      .attr('y', yFn)
       .attr('width', x.bandwidth())
-      .attr('height', d => Math.abs(y(d.value) - y(0)))
+      .attr('height', heightFn)
 
     // exit selection
     bars
